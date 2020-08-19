93632668ebd02d1fddec27e88dfe450bece773a2
mtepfer <mtepfer1@gmail.com>
2020-08-19 11:22:08 -0400

    Pretty print the duration instead of seconds

--- popup.js
+++ popup.js
@@ -1,7 +1,11 @@
 window.onload = function() {
   chrome.runtime.sendMessage({}, (time)=>{
     let div = document.createElement("div");
-    let text = document.createTextNode("You have been active on the web for " + Math.round(time/1000) + "s");
+    let dateObj = new Date(time);
+    let timeString = dateObj.getUTCHours().toString().padStart(2, '0') +
+                      ":" + dateObj.getUTCMinutes().toString().padStart(2, '0') +
+                      ":" + dateObj.getSeconds().toString().padStart(2, '0');
+    let text = document.createTextNode("You have been active on the web for " + timeString);
     div.appendChild(text);
     document.body.appendChild(div);
   });
