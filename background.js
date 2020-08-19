a9f70073c3965ed4c8a1eb5df304e167895c38d3
mtepfer <mtepfer1@gmail.com>
2020-08-19 14:17:22 -0400

    Added notification

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 const DETECTION_INTERVAL = 15;
+const TAKE_BREAK_INTERVAL_MINUTES = 1;
 chrome.idle.setDetectionInterval(DETECTION_INTERVAL);
 
 
@@ -7,11 +8,17 @@ let data = {
   activeSince: new Date()
 };
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
-  console.log("Got message");
+let alarm;
+
+function UpdateTimeActive() {
   let newActiveSince = new Date();
   data.time += newActiveSince - data.activeSince;
   data.activeSince = newActiveSince;
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
+  console.log("Got message");
+  UpdateTimeActive();
   sendResponse(data.time);
 });
 
@@ -19,9 +26,26 @@ chrome.idle.onStateChanged.addListener((newState) => {
   if(newState == "idle" || newState == "locked") {
     console.log("User became inactive");
     data.time = 0;
+    chrome.alarms.clear("notify");
   }
   else {
     console.log("User became active");
     data.activeSince = new Date();
+    chrome.alarms.create("notify", {delayInMinutes: TAKE_BREAK_INTERVAL_MINUTES});
   }
+});
+
+chrome.alarms.onAlarm.addListener(()=>{
+  console.log("Alarm trigerred")
+  UpdateTimeActive();
+  let dateObj = new Date(data.time);
+  let timeString = dateObj.getUTCHours().toString().padStart(2, '0') +
+                    ":" + dateObj.getUTCMinutes().toString().padStart(2, '0') +
+                    ":" + dateObj.getSeconds().toString().padStart(2, '0');
+  chrome.notifications.create(null, {
+    type: "basic",
+    iconUrl: "images/Time.png",
+    title: "Take a break!",
+    message: "You've been active on the web for " + timeString
+  });
 });
